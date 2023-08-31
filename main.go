a7a3b72b7ca687e2d7391711b219e33d807b05e1
jsuhula <jsuhula@miumg.edu.gt>
2023-08-31 19:17:14 +0000

    [ADD] Codigo fuente hecho en lenguaje Go

--- main.go
+++ main.go
@@ -10,11 +10,11 @@ import (
 func main() {
 
     http.HandleFunc("/analisis", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hola, esto es un microservicio!, %q", html.EscapeString(r.URL.Path))
+        fmt.Fprintf(w, "Hola! este es un Microservicio, te encuentras solicitando el path: %q", html.EscapeString(r.URL.Path))
     })
 
     http.HandleFunc("/sistemas", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Usando microservicio con Go y Docker!")
+        fmt.Fprintf(w, "Microservicio con Go y Docker By jsuhula!")
     })
 
     log.Fatal(http.ListenAndServe(":8081", nil))
