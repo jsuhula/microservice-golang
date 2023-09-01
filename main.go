5909d941e6b0f2b096645e202b9076ff78cde221
jsuhula <jsuhula@miumg.edu.gt>
2023-09-01 05:59:37 +0000

    [ADD] Archivo Principal Go

--- main.go
+++ main.go
@@ -2,21 +2,33 @@ package main
 
 import (
     "fmt"
-    "html"
     "log"
     "net/http"
+    "io/ioutil"
 )
 
 func main() {
-
     http.HandleFunc("/analisis", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hola! este es un Microservicio, te encuentras solicitando el path: %q", html.EscapeString(r.URL.Path))
+        fmt.Fprintf(w, "Hola! este es un Microservicio, te encuentras solicitando el path: %q", r.URL.Path)
     })
 
-    http.HandleFunc("/sistemas", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Microservicio con Go y Docker By jsuhula!")
+    http.HandleFunc("/colors", func(w http.ResponseWriter, r *http.Request) {
+
+        data, err := ioutil.ReadFile("colors.json")
+        if err != nil {
+            http.Error(w, "Error al leer el archivo JSON", http.StatusInternalServerError)
+            return
+        }
+
+        w.Header().Set("Content-Type", "application/json")
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+        w.WriteHeader(http.StatusOK)
+
+        w.Write(data)
+
     })
 
     log.Fatal(http.ListenAndServe(":8081", nil))
-    
 }
